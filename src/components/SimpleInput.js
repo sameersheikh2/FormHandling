edecd801f7b12d13bdbc91cb26d1e3ac62dff754
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-28 10:29:48 +0530

    added hooks to collect value and show it on console

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,26 @@
+import { useState, useRef} from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+  const nameInputRef = useRef('')
+
+  const formSubmitHandler = event =>{
+    event.preventDefault();
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredName , 'state')
+    console.log(enteredValue , 'ref')
+  }
+  
+  const enteredNameChangeHandler = event =>{
+    setEnteredName(event.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={enteredNameChangeHandler}
+        ref={nameInputRef}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
