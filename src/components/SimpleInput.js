10e2df1d013fac765b47dad0a3457b836d636a5a
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-28 10:41:45 +0530

    added simple validation on form submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,9 +3,18 @@ import { useState, useRef} from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('')
   const nameInputRef = useRef('')
+  const [enteredNameIsValid , setEnteredNameIsValid] = useState(true)
 
   const formSubmitHandler = event =>{
     event.preventDefault();
+
+    if(
+      enteredName.trim() <= 0){
+      setEnteredNameIsValid(false)
+      return;
+    }
+    setEnteredNameIsValid(true)
+
     const enteredValue = nameInputRef.current.value;
     console.log(enteredName , 'state')
     console.log(enteredValue , 'ref')
@@ -15,12 +24,15 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   }
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={enteredNameChangeHandler}
         ref={nameInputRef}/>
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
